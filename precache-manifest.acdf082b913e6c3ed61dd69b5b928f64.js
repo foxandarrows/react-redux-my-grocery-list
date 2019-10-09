e7ea955c61cef3282956c4e64ea0fd340bb0776c
saralitwak <sara.litwak@twipemobile.com>
2019-10-09 15:46:35 +0200

    Updates

---
+++ precache-manifest.acdf082b913e6c3ed61dd69b5b928f64.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d1fd07ed85fc83442c4ae12a1ab462",
+    "url": "/my-grocery-list/index.html"
+  },
+  {
+    "revision": "fe199aa4dcf67abc5b29",
+    "url": "/my-grocery-list/static/js/2.24c87227.chunk.js"
+  },
+  {
+    "revision": "e80f926aeb5c3cbfa07d",
+    "url": "/my-grocery-list/static/js/main.1f83fd0f.chunk.js"
+  },
+  {
+    "revision": "496ce45e5c38ae066462",
+    "url": "/my-grocery-list/static/js/runtime-main.371eaddd.js"
+  }
+]);
